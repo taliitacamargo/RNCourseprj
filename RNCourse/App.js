c579b608f5af51bc074e653781152e7aa702d42a
Tali <taliitacamargo@icloud.com>
2023-01-27 11:52:53 -0500

    adding initial files

---
+++ RNCourse/App.js
@@ -0,0 +1,93 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+
+
+
+export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer} >
+        <TextInput
+          style={styles.textInput}
+          placeholder='Your Course Goal!'
+          onChangeText={goalInputHandler} />
+        <Button
+          title='Add Goal'
+          onPress={addGoalHandler} />
+      </View>
+
+      <View style={styles.goalsContainer}>
+        <ScrollView>
+          {courseGoals.map((goal) => (
+            <View style={styles.goalItem} key={goal} >
+              <Text style={styles.goalText}> {goal}</Text>
+            </View>
+          ))}
+        </ScrollView >
+      </View>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+
+  },
+
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+
+  goalsContainer: {
+    flex: 3,
+  },
+
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: 'purple',
+  },
+
+  goalText: {
+    color: 'white',
+  },
+
+
+});
+
+
